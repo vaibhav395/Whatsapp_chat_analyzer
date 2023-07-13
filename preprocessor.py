20cfe8daba2419fc2ec744b71a98e1a076607315
vaibhav395 <90473158+vaibhav395@users.noreply.github.com>
2023-07-13 16:10:25 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -27,8 +27,22 @@ def preprocess(data):
     df.drop(columns=['user_message'], inplace=True)
     df['year'] = df['date'].dt.year
     df['month'] = df['date'].dt.month_name()
+    df['month_num'] = df['date'].dt.month
+    df['only_date'] = df['date'].dt.date
     df['day'] = df['date'].dt.day
     df['hour'] = df['date'].dt.hour
     df['minute'] = df['date'].dt.minute
+    df['day_name'] = df['date'].dt.day_name()
+
+    period = []
+    for hour in df['hour']:
+        if hour == 23:
+            period.append(str(hour) + "-" + str('00'))
+        elif hour == 0:
+            period.append(str('00') + "-" + str(hour + 1))
+        else:
+            period.append(str(hour) + "-" + str(hour + 1))
+
+    df['period'] = period
 
     return df
