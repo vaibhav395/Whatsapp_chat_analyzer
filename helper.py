e0de2ff0c4d5eaea94dea0ae78e156ece30e9620
vaibhav395 <90473158+vaibhav395@users.noreply.github.com>
2023-07-13 16:09:31 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,5 +1,7 @@
 from urlextract import URLExtract
 from wordcloud import WordCloud
+from collections import Counter
+import pandas as pd
 extract = URLExtract()
 def fetch_stats(selected_users , df):
     if selected_users != 'Overall':
@@ -32,6 +34,64 @@ def create_wordcloud(selected_users, df):
     if selected_users != 'Overall':
         df = df[df['users'] == selected_users]
 
+    temp = df[df['users'] != 'notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
+
     wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white')
-    df_wc = wc.generate(df['message'].str.cat(sep=" "))
+    df_wc = wc.generate(temp['message'].str.cat(sep=" "))
     return df_wc
+
+def most_common_words(selected_users, df):
+    f = open('stop_hinglish.txt', 'r')
+    stop_words = f.read()
+
+    if selected_users != 'Overall':
+        df = df[df['users'] == selected_users]
+    temp = df[df['users'] != 'notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
+
+    words = []
+    for messages in temp['message']:
+        for word in messages.lower().split():
+            if word not in stop_words:
+                words.append(word)
+
+    most_common_df = pd.DataFrame(Counter(words).most_common(20))
+    return most_common_df
+
+def monthly_timeline(selected_users, df):
+    if selected_users != 'Overall':
+        df = df[df['users'] == selected_users]
+
+    timeline = df.groupby(['year', 'month', 'month_num']).count()['message'].reset_index()
+    time = []
+    for i in range(timeline.shape[0]):
+        time.append(timeline['month'][i] + "-" + str(timeline['year'][i]))
+    timeline['time'] = time
+
+    return timeline
+
+def daily_timeline(selected_users, df):
+    if selected_users != 'Overall':
+        df = df[df['users'] == selected_users]
+    daily_timeline = df.groupby('only_date').count()['message'].reset_index()
+    return daily_timeline
+
+def week_activity_map(selected_users, df):
+    if selected_users != 'Overall':
+        df = df[df['users'] == selected_users]
+
+    return df['day_name'].value_counts()
+
+def month_activity_map(selected_users, df):
+    if selected_users != 'Overall':
+        df = df[df['users'] == selected_users]
+
+    return df['month'].value_counts()
+
+def activity_heatmap(selected_users, df):
+    if selected_users != 'Overall':
+        df = df[df['users'] == selected_users]
+    user_heatmap = df.pivot_table(index='day_name', columns='period', values='message', aggfunc='count').fillna(0)
+    return user_heatmap
+
